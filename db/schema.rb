20ea79a0fa12f8e7e970d65a8d28ccd4dda6b2ca
danielburridge <daniel.burridge@protonmail.com>
2018-04-18 23:53:40 +0100

    added category, gdprarticle, gdprrecital, jerseylaw and category models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418213201) do
+ActiveRecord::Schema.define(version: 20180418224951) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "gdprarticles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer "jerseylaw_id"
+    t.integer "chapter_id"
+    t.string "chapter_romannum"
+    t.string "chapter_title"
+    t.integer "section_id"
+    t.integer "section_num"
+    t.string "section_title"
+    t.integer "article_id"
+    t.integer "article_num"
+    t.string "article_title"
+    t.text "article_body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "gdprrecitals", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer "jerseylaw_id"
+    t.integer "gdprarticles_id"
+    t.integer "recital_id"
+    t.integer "recital_num"
+    t.string "recital_title"
+    t.text "recital_body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jerseylaws", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer "category_id"
+    t.integer "part_id"
+    t.integer "part_num"
+    t.string "part_title"
+    t.integer "article_id"
+    t.integer "article_num"
+    t.string "article_title"
+    t.text "article_body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "email", default: "", null: false
---
+++ test/models/jerseylaw_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class JerseylawTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
