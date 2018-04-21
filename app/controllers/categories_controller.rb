ef8f454ebd4b81173ad626e0b128d4f407cc718c
danielburridge <daniel.burridge@protonmail.com>
2018-04-21 16:38:50 +0100

    new feature branch for building controllers and adding friendly_id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,9 @@
 class CategoriesController < ApplicationController
   def index
+  	@categories = Category.all.order('title ASC')
   end
 
   def show
+  	@category = Category.friendly.find(params[:id])
   end
 end
