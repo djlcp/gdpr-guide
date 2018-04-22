4c36860ab984aad5eb688d981ce83791cec3f185
danielburridge <daniel.burridge@protonmail.com>
2018-04-22 01:25:13 +0100

    added devise invitable functionality

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 gem 'devise'
+gem 'devise_invitable', '~> 1.7.0'
 gem 'friendly_id', '~> 5.1.0'
 
 # Use Capistrano for deployment
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   
-	extend FriendlyId
-  friendly_id :email, use: :slugged
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+	  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :invitable, :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
